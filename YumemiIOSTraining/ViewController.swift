ad952732ada2e697444172d80adda87adfa90463
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-08-04 12:42:18 +0900

    Session1実装

---
+++ YumemiIOSTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YumemiIOSTraining
+//
+//  Created by 今村京平 on 2021/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
