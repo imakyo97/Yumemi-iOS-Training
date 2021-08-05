84d5272cc49160d4031f32cb3f3b3e136584d20f
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-08-05 11:53:47 +0900

    Session2実装

--- YumemiIOSTraining/ViewController.swift
+++ YumemiIOSTraining/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private weak var weatherImageView: UIImageView!
+
+    enum Weather {
+        static let sunny = "sunny"
+        static let cloudy = "cloudy"
+        static let rainy = "rainy"
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
+    @IBAction func didTapReloadButton(_ sender: Any) {
+        let stringWeather = YumemiWeather.fetchWeather()
+        switch stringWeather {
+        case Weather.sunny:
+            let sunnyImage = UIImage(named: Weather.sunny)
+            weatherImageView.image = sunnyImage
+            weatherImageView.tintColor = .red
+        case Weather.cloudy:
+            let cloudyImage = UIImage(named: Weather.cloudy)
+            weatherImageView.image = cloudyImage
+            weatherImageView.tintColor = .gray
+        case Weather.rainy:
+            let rainyImage = UIImage(named: Weather.rainy)
+            weatherImageView.image = rainyImage
+            weatherImageView.tintColor = .blue
+        default:
+            return
+        }
+    }
 }
 
