5c2eaa96cf5097f1887cbaa61c36bcdbbfa12647
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-08-06 20:55:36 +0900

    Session3実装

--- YumemiIOSTraining/ViewController.swift
+++ YumemiIOSTraining/ViewController.swift
@@ -23,23 +23,50 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didTapReloadButton(_ sender: Any) {
-        let stringWeather = YumemiWeather.fetchWeather()
-        switch stringWeather {
-        case Weather.sunny:
-            let sunnyImage = UIImage(named: Weather.sunny)
-            weatherImageView.image = sunnyImage
-            weatherImageView.tintColor = .red
-        case Weather.cloudy:
-            let cloudyImage = UIImage(named: Weather.cloudy)
-            weatherImageView.image = cloudyImage
-            weatherImageView.tintColor = .gray
-        case Weather.rainy:
-            let rainyImage = UIImage(named: Weather.rainy)
-            weatherImageView.image = rainyImage
-            weatherImageView.tintColor = .blue
-        default:
-            return
+        do {
+            let stringWeather = try YumemiWeather.fetchWeather(at: "tokyo")
+            switch stringWeather {
+            case Weather.sunny:
+                let sunnyImage = UIImage(named: Weather.sunny)
+                weatherImageView.image = sunnyImage
+                weatherImageView.tintColor = .red
+            case Weather.cloudy:
+                let cloudyImage = UIImage(named: Weather.cloudy)
+                weatherImageView.image = cloudyImage
+                weatherImageView.tintColor = .gray
+            case Weather.rainy:
+                let rainyImage = UIImage(named: Weather.rainy)
+                weatherImageView.image = rainyImage
+                weatherImageView.tintColor = .blue
+            default:
+                return
+            }
+        } catch {
+            switch error {
+            case YumemiWeatherError.invalidParameterError:
+                presentAlertController(message: "無効なパラメータエラーです。")
+            case YumemiWeatherError.unknownError:
+                presentAlertController(message: "不明なエラーです")
+            default:
+                fatalError()
+            }
         }
+    }
+
+    private func presentAlertController(message: String) {
+        let alert = UIAlertController(
+            title: "読み込みに失敗しました。",
+            message: nil,
+            preferredStyle: .alert
+        )
+        let action = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil
+        )
+        alert.message = message
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 }
 
